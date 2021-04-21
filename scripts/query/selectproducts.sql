35e43f95e7768702e916d1c6db11dc20782c7b66
Porti <Pasha19682000d@gmail.com>
2021-04-21 14:32:52 +0300

    update

---
+++ scripts/query/selectproducts.sql
@@ -0,0 +1,5 @@
+SELECT  Producers.ProducerName AS Producer,
+        SUM(WarehouseProduct.Quantity) AS Product_quantity
+FROM Products JOIN Producers ON Products.ProducerId = Producers.Id
+              JOIN WarehouseProduct ON Products.Id = WarehouseProduct.ProductId
+GROUP BY Producers.ProducerName
--- scripts/tables/products.sql
+++ scripts/tables/products.sql
@@ -6,7 +6,8 @@ CREATE TABLE [dbo].[Products]
 	[ProductId] INT IDENTITY   (1, 1) NOT NULL PRIMARY KEY CLUSTERED, 
     [ProductName] NVARCHAR(100) NOT NULL,
 	[manufacturerId] INT NOT NULL  REFERENCES [dbo].[manufacturer] ([manufactrerId]),
-	[warehouseId] INT NOT NULL  REFERENCES [dbo].[Warehouses] ([warehouseId])
+	[warehouseId] INT NOT NULL  REFERENCES [dbo].[Warehouses] ([warehouseId]),
+	[amount] INT NULL
 );
 
 	GO
